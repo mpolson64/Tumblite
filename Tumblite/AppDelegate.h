139c60c0f8266e8d2ee24bf04b459fa76f36e001
Miles Olson <mpolson64@fb.com>
2018-06-27 10:21:38 -0700

    Initial Commit

---
+++ Tumblite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tumblite
+//
+//  Created by Miles Olson on 6/27/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tumblite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblite
+//
+//  Created by Miles Olson on 6/27/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
